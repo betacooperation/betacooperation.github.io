0705744c0d60d66a3e3ba7ac0a043412767cbe83
Diederick Lawson <webmaster@altovista.nl>
2018-01-15 13:04:03 +0100

    Updated site

---
+++ javascripts/carousel.js
@@ -0,0 +1,70 @@
+$(function() {
+  var timer = null;
+
+  function setTimer() {
+    if(timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    timer = setTimeout(nextSlide, 15000);
+  }
+
+  function nextSlide() {
+    var slides = $(".carousel .carousel__item");
+    var slideIndex;
+
+    for(var i = 0; i < slides.length; i ++) {
+      if($(slides[i]).hasClass("carousel__item--active")) {
+        slideIndex = i;
+        break;
+      }
+    }
+
+    var nextSlideIndex = slideIndex + 1;
+    if(nextSlideIndex >= slides.length) nextSlideIndex = 0;
+
+    gotoSlide(nextSlideIndex);
+
+    // neeeext
+    setTimer();
+  }
+
+  function gotoSlide(nextSlideIndex) {
+    var slides = $(".carousel .carousel__item");
+    var slideIndex;
+
+    for(var i = 0; i < slides.length; i ++) {
+      if($(slides[i]).hasClass("carousel__item--active")) {
+        slideIndex = i;
+        break;
+      }
+    }
+
+    var currentSlide = $(slides[slideIndex]);
+    var nextSlide = $(slides[nextSlideIndex]);
+
+    var left = nextSlide.position().left;
+
+    $(".carousel .carousel__container").css("transform", "translateX(-" + left + "px)");
+
+    currentSlide.removeClass("carousel__item--active");
+    nextSlide.addClass("carousel__item--active");
+
+    var pages = $(".carousel__page");
+    pages.removeClass("carousel__page--active");
+    $(pages[nextSlideIndex]).addClass("carousel__page--active");
+  }
+
+  function initPageButtons() {
+    $(".carousel__page").click(function(event) {
+      event.preventDefault();
+      var page = $(event.currentTarget);
+      gotoSlide(page.data("index"));
+    });
+  }
+
+  // let's go!
+  setTimer();
+  initPageButtons();
+});
